b339d541e9ce431651fbe549019fe500c1a8a862
cpoopc <303727604@qq.com>
2015-08-27 14:20:16 +0800

    add MIT License

--- scrollablelayoutlib/src/main/java/com/cpoopc/scrollablelayoutlib/ScrollableHelper.java
+++ scrollablelayoutlib/src/main/java/com/cpoopc/scrollablelayoutlib/ScrollableHelper.java
@@ -1,3 +1,26 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 cpoopc
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package com.cpoopc.scrollablelayoutlib;
 
 import android.view.View;
@@ -6,7 +29,7 @@ import android.widget.AdapterView;
 import android.widget.ScrollView;
 
 /**
- * Created by cpoopc on 2015-02-10.
+ * Created by cpoopc(303727604@qq.com) on 2015-02-10.
  */
 public class ScrollableHelper {
 
